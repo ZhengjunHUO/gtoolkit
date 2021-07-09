f564a42deea39138b50a4b49ed360b745d627055
ZhengjunHUO <firelouiszj@hotmail.com>
2021-07-09 09:04:39 +0200

    add func in IntSlice

--- IntSlice.go
+++ IntSlice.go
@@ -9,6 +9,15 @@ func IntSliceToString(a []int) string {
 	return strings.Trim(strings.Join(strings.Split(fmt.Sprint(a), " "), ""), "[]")	
 }
 
+// Return "new" slice with the specified element removed
+// The origin slice is no longer safe to use
 func IntSliceRemove(s []int, idx int) []int {
 	return append(s[:idx], s[idx+1:]...)
+}
+
+// Remove the element in place
+func IntSliceRemoveInplace(s *[]int, idx int) {
+        copy((*s)[idx:], (*s)[idx+1:])
+        (*s)[len(*s)-1] = 0
+        *s = (*s)[:len(*s)-1]
 }
